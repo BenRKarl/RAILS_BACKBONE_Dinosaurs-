b8b8e70eaa3949afc5f7990c41002bfaed14e161
Ben Karl <benrkarl@gmail.com>
2014-07-06 12:34:19 -0400

    add form for new bone input in dino template

--- backbone/views/dinos/dinoView.js
+++ backbone/views/dinos/dinoView.js
@@ -6,24 +6,21 @@ BonesApp.Views.DinoView = Backbone.View.extend({
     this.listenTo(this.model, 'change', this.render);
   },
   tagName: 'li',
+  addBoneTemplate: _.template($('.edit-bone-template').html()),
   editTemplate: _.template($('.edit-dino-template').html()),
   template: _.template($('.dino-template').html()),
   render: function(){
     var renderedHTML = this.template(this.model.attributes);
     this.$el.html(renderedHTML);
-debugger;
     var boneCollectionView = new BonesApp.Views.BoneListView({
       collection: this.model.get('bones'),
       el: this.$el.find('.bones-list')
     });
-
-
     return this;
   },
   events:{
     'click [data-action="delete"]': 'deleteDino',
-    'click [data-action="edit"]': 'renderDinoEditForm',
-    'click [data-action="add-bone"]': 'renderNewBoneForm'
+    'click [data-action="edit"]': 'renderDinoEditForm'
   },
   deleteDino: function(){
     this.model.destroy();
@@ -46,8 +43,5 @@ debugger;
       // that.model.save();
     })
     return this;
-  },
-  renderNewBoneForm: function(){
-    var that = this;
   }
 });
